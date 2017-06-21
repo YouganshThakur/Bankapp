eab26e1207882b04365aad8a94b1e4b3c113bae7
Yougansh Thakur <Y.thakur21@yahoo.com>
2017-06-22 02:56:33 +0530

    budget dashboard, budget calculator ui and dashboard half

--- app/src/main/java/com/inbuiltsolutions/bankapp/BudgetHome.java
+++ app/src/main/java/com/inbuiltsolutions/bankapp/BudgetHome.java
@@ -1,68 +1,115 @@
 package com.inbuiltsolutions.bankapp;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class BudgetHome extends AppCompatActivity {
+import static android.R.attr.data;
 
-    PieChart pieChart ;
-    ArrayList<Entry> entries ;
-    ArrayList<String> PieEntryLabels ;
-    PieDataSet pieDataSet ;
-    PieData pieData ;
+public class BudgetHome extends AppCompatActivity implements OnChartValueSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_home);
-        pieChart = (PieChart) findViewById(R.id.chart1);
 
-        entries = new ArrayList<>();
+        //int[] colors={Color.parseColor("#FF5722"),Color.parseColor("#EF5350"),Color.parseColor("#F06292"),
+        //        Color.parseColor("#673AB7"),Color.parseColor("#3D5AFE"),Color.parseColor("#FF9800"),
+          //      Color.parseColor("#3D5AFE"),Color.parseColor("#FFEB3B")};
+        PieChart pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
 
-        PieEntryLabels = new ArrayList<String>();
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(8f, 0));
+        yvalues.add(new Entry(15f, 1));
+        yvalues.add(new Entry(12f, 2));
+        yvalues.add(new Entry(25f, 3));
+        yvalues.add(new Entry(23f, 4));
+        yvalues.add(new Entry(17f, 5));
+        yvalues.add(new Entry(21f,6));
 
-        AddValuesToPIEENTRY();
+        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
 
-        AddValuesToPieEntryLabels();
 
-        pieDataSet = new PieDataSet(entries, "");
+        ArrayList<String> xVals = new ArrayList<String>();
 
-        pieData = new PieData(PieEntryLabels, pieDataSet);
+        xVals.add("January");
+        xVals.add("February");
+        xVals.add("March");
+        xVals.add("April");
+        xVals.add("May");
+        xVals.add("June");
+        xVals.add("October");
 
-        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
 
-        pieChart.setData(pieData);
+        pieChart.setData(data);
+        pieChart.setDescription("");
 
-        pieChart.animateY(3000);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setTransparentCircleRadius(25f);
+        pieChart.setHoleRadius(85f);
+
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        data.setValueTextSize(0f);
+        data.setValueTextColor(Color.DKGRAY);
+        pieChart.setOnChartValueSelectedListener(this);
+
+        pieChart.setCenterText("Pie charts\n1000"+getString(R.string.Rs));
+        pieChart.setCenterTextSize(22);
+        pieChart.setCenterTextTypeface(Typeface.DEFAULT_BOLD);
+
+        pieChart.animateXY(1400, 1400);
+
+
+        Legend l = pieChart.getLegend();
+        l.setFormSize(12f); // set the size of the legend forms/shapes
+        l.setForm(Legend.LegendForm.CIRCLE); // set what type of form/shape should be used
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART_CENTER);
+        l.setTextSize(14f);
+        l.setTextColor(Color.BLACK);
+        l.setXEntrySpace(0f); // set the space between the legend entries on the x-axis
+        l.setYEntrySpace(5f); // set the space between the legend entries on the y-axis
+
+
+
 
     }
 
-    public void AddValuesToPIEENTRY(){
+    @Override
+    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
 
-        entries.add(new BarEntry(2f, 0));
-        entries.add(new BarEntry(4f, 1));
-        entries.add(new BarEntry(6f, 2));
-        entries.add(new BarEntry(3f, 3));
-        entries.add(new BarEntry(7f, 4));
-        entries.add(new BarEntry(3f, 5));
-        entries.add(new BarEntry(3f, 6));
+        if (e == null)
+            return;
     }
 
-    public void AddValuesToPieEntryLabels(){
-
-        PieEntryLabels.add("January");
-        PieEntryLabels.add("February");
-        PieEntryLabels.add("March");
-        PieEntryLabels.add("April");
-        PieEntryLabels.add("May");
-        PieEntryLabels.add("June");
-        PieEntryLabels.add("June");
+    @Override
+    public void onNothingSelected() {
+       // Log.i("PieChart", "nothing selected");
     }
 }
