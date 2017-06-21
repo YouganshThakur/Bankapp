03d16ca30b938b4f88233194be77926fa6b9a030
Yougansh Thakur <Y.thakur21@yahoo.com>
2017-06-21 19:02:00 +0530

    Initial commit

---
+++ app/src/main/java/com/inbuiltsolutions/bankapp/BudgetHome.java
@@ -0,0 +1,68 @@
+package com.inbuiltsolutions.bankapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class BudgetHome extends AppCompatActivity {
+
+    PieChart pieChart ;
+    ArrayList<Entry> entries ;
+    ArrayList<String> PieEntryLabels ;
+    PieDataSet pieDataSet ;
+    PieData pieData ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_home);
+        pieChart = (PieChart) findViewById(R.id.chart1);
+
+        entries = new ArrayList<>();
+
+        PieEntryLabels = new ArrayList<String>();
+
+        AddValuesToPIEENTRY();
+
+        AddValuesToPieEntryLabels();
+
+        pieDataSet = new PieDataSet(entries, "");
+
+        pieData = new PieData(PieEntryLabels, pieDataSet);
+
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        pieChart.setData(pieData);
+
+        pieChart.animateY(3000);
+
+    }
+
+    public void AddValuesToPIEENTRY(){
+
+        entries.add(new BarEntry(2f, 0));
+        entries.add(new BarEntry(4f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(3f, 3));
+        entries.add(new BarEntry(7f, 4));
+        entries.add(new BarEntry(3f, 5));
+        entries.add(new BarEntry(3f, 6));
+    }
+
+    public void AddValuesToPieEntryLabels(){
+
+        PieEntryLabels.add("January");
+        PieEntryLabels.add("February");
+        PieEntryLabels.add("March");
+        PieEntryLabels.add("April");
+        PieEntryLabels.add("May");
+        PieEntryLabels.add("June");
+        PieEntryLabels.add("June");
+    }
+}
